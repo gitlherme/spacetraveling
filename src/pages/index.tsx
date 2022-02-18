7f788364e322c77114f2a74279ba5f070d776a30
= <=>
2022-02-18 02:04:00 -0300

    [WIP] feat: list posts.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
 import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import Link from 'next/link';
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -22,13 +28,37 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): JSX.Element {
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
   return (
     <>
+      <Head>
+        <title>Spacetraveling</title>
+      </Head>
       <main className={styles.main}>
         <div className={commonStyles.container}>
           <div className={commonStyles.content}>
-            <h1>Olá</h1>
+            <div className={styles.posts}>
+              {postsPagination.results.map(post => (
+                <div key={post.uid} className={styles.post}>
+                  <h2 className={styles.title}>
+                    <Link href={`/post/${post.uid}`}>
+                      <a>{post.data.title}</a>
+                    </Link>
+                  </h2>
+                  <p>{post.data.subtitle}</p>
+                  <div className={styles.information}>
+                    <span>
+                      <FiCalendar />
+                      {post.first_publication_date}
+                    </span>
+                    <span>
+                      <FiUser />
+                      {post.data.author}
+                    </span>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </main>
@@ -36,8 +66,46 @@ export default function Home(): JSX.Element {
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
-//   return console.log('postsResponse');
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: [
+        'posts.title',
+        'posts.subtitle',
+        'posts.author',
+        'posts.uid',
+        'posts.first_publication_date',
+      ],
+      pageSize: 1,
+    }
+  );
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd/MM/yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  const postsPagination: PostPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  };
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
